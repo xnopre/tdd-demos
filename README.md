06309ec5e3af923558eea2fd412a3ae2f46bd423
Xavier NOPRE <xnopre@gmail.com>
2018-11-14 17:04:34 +0100

    Ajout du sujet du Kata dans le README

---
+++ README.md
@@ -0,0 +1,22 @@
+# Agile Grenoble 2018 : TDD
+
+Le sources "live" de ma session 
+**TDD :  la base pour faire du développement agile**
+à Agile Grenoble 2018.
+
+# Diaporama
+[Le diaporama de la session est ici.](https://docs.google.com/presentation/d/1763-z_Ag41U5o2-26hd2-gcb7RM1lXbK08JkbZX3W5U/edit?usp=sharing)
+
+# Sujet du Kata
+
+Calcul de prix :
+* HT : 3 articles à 1,21 € + 0 % ⇒ “3.63 €”
+* TTC : 3 articles à 1,21 €  + 5 % ⇒ “3.81 €”
+* TTC : 3 articles à 1,21 €  +20 % ⇒ “4.36 €”
+> → “TDC” = “Correction Pilotée par les Tests”
+
+On ajoute des réductions :
+* \> 1000 € → Remise 3% :
+  * Ex : 5 x 345,00 € + taxe 10% → “1840.58 €”
+* \> 5000 € → Remise 5% :
+  * Ex : 5 x 1299,00 € + taxe 10% → “6787.28 €”
---
+++ src/test/java/xnopre/PriceCalculatorTest.java
@@ -0,0 +1,40 @@
+package xnopre;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PriceCalculatorTest {
+
+    PriceCalculator priceCalculator = new PriceCalculator();
+
+    @Test
+    public void should_calculate_price_without_tax() {
+        String price = priceCalculator.calculate(3, 1.21, 0);
+        assertThat(price).isEqualTo("3.63 €");
+    }
+
+    @Test
+    public void should_calculate_price_with_tax() {
+        String price = priceCalculator.calculate(3, 1.21, 5);
+        assertThat(price).isEqualTo("3.81 €");
+    }
+
+    @Test
+    public void should_calculate_price_with_tax_20() {
+        String price = priceCalculator.calculate(3, 1.21, 20);
+        assertThat(price).isEqualTo("4.36 €");
+    }
+
+    @Test
+    public void should_calculate_price_with_discount_1000() {
+        String price = priceCalculator.calculate(5, 345.00, 10);
+        assertThat(price).isEqualTo("1840.58 €");
+    }
+
+    @Test
+    public void should_calculate_price_with_discount_5000() {
+        String price = priceCalculator.calculate(5, 1299.00, 10);
+        assertThat(price).isEqualTo("6787.28 €");
+    }
+}
